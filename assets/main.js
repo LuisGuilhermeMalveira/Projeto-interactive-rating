30541c5da3f08661dc2a03c90bef6eb7f6a28528
Luís Guilherme Theóphilo Malveira <97563186+LuisGuilhermeMalveira@users.noreply.github.com>
2022-09-26 23:36:26 -0300

    adicionando interatividade botões.

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,43 @@
 const caixa1 = document.querySelector(".caixa1");
-
 const caixa2 = document.querySelector(".caixa2");
-
 const botaoSubmit = document.getElementById("submit");
-
 const simboloX = document.querySelector(".simbolox");
-
 const estrela = document.querySelector(".estrela");
-
 const estrelacinza = document.querySelector(".estrelacinza");
-
 const estrelalaranja = document.querySelector(".estrelalaranja");
-
-botaoSubmit.addEventListener("click", () => {
-	caixa2.classList.remove("hidden");
-	caixa1.style.display = "none";
-});
+const resultadoNota = document.getElementById("resultadoNota");
+const botoesNota = document.querySelectorAll(".botoesNota");
 
 simboloX.addEventListener("click", () => {
 	caixa2.classList.add("hidden");
 	caixa1.style.display = "flex";
 });
 
+var clickEstrela = false;
+
 estrela.addEventListener("click", () => {
-	estrelacinza.classList.add("hidden");
-	estrelalaranja.classList.remove("hidden");
+	clickEstrela = !clickEstrela;
+	if (clickEstrela) {
+		estrelacinza.classList.add("hidden");
+		estrelalaranja.classList.remove("hidden");
+	} else {
+		estrelacinza.classList.remove("hidden");
+		estrelalaranja.classList.add("hidden");
+	}
+});
+
+botaoSubmit.addEventListener("click", () => {
+	caixa2.classList.remove("hidden");
+	caixa1.style.display = "none";
+});
+
+botaoSubmit.disabled = true;
+
+botoesNota.forEach((nota) => {
+	nota.addEventListener("click", () => {
+		botaoSubmit.classList.add("botaosubmithover");
+		botaoSubmit.disabled = false;
+		resultadoNota.innerHTML = nota.innerHTML;
+		console.log(nota);
+	});
 });
